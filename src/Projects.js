afbced78f34bcba3047b0debd6bc780622906a6f
Vembarasan Nagarajan <vembarasugn@gmil.com>
2023-09-20 13:52:56 +0530

    Pricing table app updates

--- src/Projects.js
+++ src/Projects.js
@@ -201,7 +201,7 @@ function Projects(){
     {
       id:"20",
       Projectname:"R Shiny Application - BCL Data.",
-      description:"This app will be helpful to analyse data analysis.I have built this app using R and R shiny.In this app you can explore few ideas about how you can analyse data and visualize them. ",
+      description:"This app will helpful to analyse data analysis.I have built this app using R and R shiny.In this app you can explore few ideas about how you can analyse data and visualize them. ",
       Technologies:" R Language . R Studio . ShinyIO . Posit ",
       sourcecode:"https://github.com/vembarasugn?tab=repositories",
       livereview:"https://myrshiny1.shinyapps.io/Appshiny/",
